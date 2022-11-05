650e6779ab98ee11b060d8d9b7b15128269d5101
schooldev49 <102310270+schooldev49@users.noreply.github.com>
2022-11-04 22:51:47 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 
 typedef int i;
-typedef long long id;
+typedef unsigned long long int id;
 typedef bool b;
 #define syn ios::sync_with_stdio;
 #define w(cond) while(cond);
@@ -13,7 +13,7 @@ int main() {
   cin.tie(0);
     
     id pass;
-  cout << "Enter your pin to see how fast people can crack it. 9+ Digits wont work due to Repl limitations but no one has that on their mobile devices. No data is stored and this is open source so you can check the source code (it sucks but whatever)" << "\n";
+  cout << "Enter your pin to see how fast people can crack it." << "\n";
   cin >> pass;
   cout << pass << "(this is input) \n";
   id current = 0;
@@ -29,8 +29,8 @@ bool e = false;
     if (current == pass){
     auto stop = chrono::high_resolution_clock::now();
 auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
-  
-      cout << "Cracked! Pin was " << current << ". Cracked in "<< duration.count() << "miliseconds";
+  double e = duration.count();
+      cout << "Cracked! Pin was " << current << ". Cracked in about "<< e / 1000000 << "seconds";
    
 
 
